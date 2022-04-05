9d37c06b5b49b071167b545880ba154193771b34
lkouami <rkouami@outlook.com>
2022-04-05 13:51:57 -0700

    allowed hosts

--- CarRentalApp/settings.py
+++ CarRentalApp/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-93oa2vyeyzjit!facka=2&0_(eudb@5m808pd8g7mas!t$c1-c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+    'genpk.herokuapp.com'
+]
 
 
 # Application definition
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
