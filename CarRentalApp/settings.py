a1e30e64a840e2bc5d51e1e52888c16a798d3ef1
Lucas Kouami <rkouami@outlook.com>
2022-06-23 19:40:37 +0000

    return to mediafiles

--- CarRentalApp/settings.py
+++ CarRentalApp/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
 AUTH_USER_MODEL = "authentication.CustomUser"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
